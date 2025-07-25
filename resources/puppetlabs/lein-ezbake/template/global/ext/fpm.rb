170b78098d16721135eee4b0a5f4724995641b73
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2025-07-25 19:19:55 +0200

    Patch java_bin if overridden
    In 9768a77de95ff19f66f4b328fc60efe69f5f3a6b the java binary was
    overridden, but this didn't have a point because it's not available when
    the files are rendered. They only have access to the ezbake config.
    
    This takes an approach of patching the files after the fact. It writes
    out backup files that are restored after. That is to deal with the fact
    that our build process isn't clean and is reused between multiple
    builds.
    
    Fixes: 9768a77de95f ("Use virtual Java packages on Red Hat and set java_bin")

--- resources/puppetlabs/lein-ezbake/template/global/ext/fpm.rb
+++ resources/puppetlabs/lein-ezbake/template/global/ext/fpm.rb
@@ -4,6 +4,40 @@ require 'open3'
 require 'optparse'
 require 'ostruct'
 
+def patch_files(options)
+  if options.java_bin == EZBake::Config[:java_bin]
+    yield
+  else
+    suffix = '.backup'
+    [
+      # Debian
+      '/etc/default/puppet*',
+      '/lib/systemd/system/puppet*.service',
+      # RPM
+      '/usr/lib/systemd/system/puppet*.service',
+      '/etc/sysconfig/puppet*',
+    ].each do |path|
+      Dir.glob(File.join(options.chdir, path)).each do |real_path|
+        content = File.read(real_path)
+        next unless content.include?(EZBake::Config[:java_bin])
+
+        warn "Copying #{real_path} to #{real_path}#{suffix}"
+        FileUtils.cp(real_path, "#{real_path}#{suffix}")
+        warn "Patching #{real_path} to use #{options.java_bin}"
+        File.write(real_path, content.gsub(EZBake::Config[:java_bin], options.java_bin))
+      end
+    end
+
+    yield
+
+    Dir.glob(File.join(options.chdir, '**', "*#{suffix}")).each do |path|
+      original = File.join(File.dirname(path), File.basename(path, suffix))
+      warn "Restoring #{path} to #{original}"
+      FileUtils.mv(path, original)
+    end
+  end
+end
+
 options = OpenStruct.new
 
 # ezbake.rb is rendered from
@@ -432,22 +466,24 @@ if options.debug
   puts "#{Dir.pwd}"
 end
 
-# fpm sends all output to stdout
-out, _, stat = Open3.capture3("#{fpm_editor} fpm #{fpm_opts.join(' ')}")
-fail "Error trying to run FPM for #{options.dist}!\n#{out}" unless stat.success?
-
-puts "#{out}"
-
-if options.termini
-  if options.debug
-    puts "=========================="
-    puts "FPM COMMAND"
-    puts "fpm #{termini_opts.join(' ')}"
-    puts "=========================="
-  end
-
+patch_files(options) do
   # fpm sends all output to stdout
-  out, _, stat = Open3.capture3("fpm #{termini_opts.join(' ')}")
-  fail "Error trying to run FPM for the termini for #{options.dist}!\n#{out}" unless stat.success?
+  out, _, stat = Open3.capture3("#{fpm_editor} fpm #{fpm_opts.join(' ')}")
+  fail "Error trying to run FPM for #{options.dist}!\n#{out}" unless stat.success?
+
   puts "#{out}"
+
+  if options.termini
+    if options.debug
+      puts "=========================="
+      puts "FPM COMMAND"
+      puts "fpm #{termini_opts.join(' ')}"
+      puts "=========================="
+    end
+
+    # fpm sends all output to stdout
+    out, _, stat = Open3.capture3("fpm #{termini_opts.join(' ')}")
+    fail "Error trying to run FPM for the termini for #{options.dist}!\n#{out}" unless stat.success?
+    puts "#{out}"
+  end
 end
