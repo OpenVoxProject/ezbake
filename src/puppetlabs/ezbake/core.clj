OpenVoxProject/ezbake

commit b2de7c7a2e7704491a08665a9f53ded8009aa109

Drop service wrappers
These service wrappers only replicate the default systemd behavior.
Running it directly simplifies it greatly because it also drops the need
to manage the rundir, which systemd will now take care of for us.

resources/puppetlabs/lein-ezbake/template/global/ext/redhat/ezbake.service.erb
@@ -19,14 +19,14 @@ Wants=<%= EZBake::Config[:start_after].map {|dep| "#{dep}.service" }.join(" ") %
 <% end -%>
 
 [Service]
-Type=forking
+Type=exec
+LogsDirectory=puppetlabs/<%= EZBake::Config[:real_name] %>
 EnvironmentFile=/etc/sysconfig/<%= EZBake::Config[:project] %>
 User=<%= EZBake::Config[:user] %>
 TimeoutStartSec=<%= EZBake::Config[:start_timeout] %>
 TimeoutStopSec=<%= EZBake::Config[:stop_timeout] %>
 Restart=on-failure
 StartLimitBurst=5
-PIDFile=/run/puppetlabs/<%= EZBake::Config[:real_name] %>/<%= EZBake::Config[:real_name] %>.pid
 PrivateTmp=true
 
 # https://tickets.puppetlabs.com/browse/EZ-129
@@ -50,9 +50,15 @@ ExecStartPre=<%= action %>
 <% end -%>
 <% end -%>
 
-ExecReload=/opt/puppetlabs/server/apps/<%= EZBake::Config[:real_name] %>/bin/<%= EZBake::Config[:real_name] %> reload
-ExecStart=/opt/puppetlabs/server/apps/<%= EZBake::Config[:real_name] %>/bin/<%= EZBake::Config[:real_name] %> start
-ExecStop=/opt/puppetlabs/server/apps/<%= EZBake::Config[:real_name] %>/bin/<%= EZBake::Config[:real_name] %> stop
+ExecStart="$JAVA_BIN" $JAVA_ARGS $LOG_APPENDER \
+  '-XX:OnOutOfMemoryError=kill -9 %p' -XX:+CrashOnOutOfMemoryError \
+  -XX:ErrorFile="$LOGS_DIRECTORY/<%= EZBake::Config[:real_name] %>_err_pid%p.log" \
+  -cp "$INSTALL_DIR/<%= EZBake::Config[:uberjar_name] %>" \
+  clojure.main \
+  -m <%= EZBake::Config[:main_namespace] %> \
+  --config "$CONFIG" \
+  --bootstrap-config "$BOOTSTRAP_CONFIG" \
+  $TK_ARGS
 
 KillMode=process
 

resources/puppetlabs/lein-ezbake/template/global/ext/debian/ezbake.service.erb
@@ -19,14 +19,14 @@ Wants=<%= EZBake::Config[:start_after].map {|dep| "#{dep}.service" }.join(" ") %
 <% end -%>
 
 [Service]
-Type=forking
+Type=exec
+LogsDirectory=puppetlabs/<%= EZBake::Config[:real_name] %>
 EnvironmentFile=/etc/default/<%= EZBake::Config[:project] %>
 User=<%= EZBake::Config[:user] %>
 TimeoutStartSec=<%= EZBake::Config[:start_timeout] %>
 TimeoutStopSec=<%= EZBake::Config[:stop_timeout] %>
 Restart=on-failure
 StartLimitBurst=5
-PIDFile=/run/puppetlabs/<%= EZBake::Config[:real_name] %>/<%= EZBake::Config[:real_name] %>.pid
 PrivateTmp=true
 
 # https://tickets.puppetlabs.com/browse/EZ-129
@@ -50,9 +50,15 @@ ExecStartPre=<%= action %>
 <% end -%>
 <% end -%>
 
-ExecReload=/opt/puppetlabs/server/apps/<%= EZBake::Config[:real_name] %>/bin/<%= EZBake::Config[:real_name] %> reload
-ExecStart=/opt/puppetlabs/server/apps/<%= EZBake::Config[:real_name] %>/bin/<%= EZBake::Config[:real_name] %> start
-ExecStop=/opt/puppetlabs/server/apps/<%= EZBake::Config[:real_name] %>/bin/<%= EZBake::Config[:real_name] %> stop
+ExecStart="$JAVA_BIN" $JAVA_ARGS $LOG_APPENDER \
+  '-XX:OnOutOfMemoryError=kill -9 %p' -XX:+CrashOnOutOfMemoryError \
+  -XX:ErrorFile="$LOGS_DIRECTORY/<%= EZBake::Config[:real_name] %>_err_pid%p.log" \
+  -cp "$INSTALL_DIR/<%= EZBake::Config[:uberjar_name] %>" \
+  clojure.main \
+  -m <%= EZBake::Config[:main_namespace] %> \
+  --config "$CONFIG" \
+  --bootstrap-config "$BOOTSTRAP_CONFIG" \
+  $TK_ARGS
 
 KillMode=process
 

resources/puppetlabs/lein-ezbake/staging-templates/project_data.yaml.mustache
@@ -29,8 +29,6 @@ templates:
     target: ext/debian/{{{project}}}.service_file
   - source: ext/ezbake.logrotate.conf.erb
     target: ext/{{{project}}}.logrotate.conf
-  - source: ext/ezbake.tmpfiles.conf.erb
-    target: ext/{{{project}}}.tmpfiles.conf
   - source: ext/ezbake.needrestart.conf.erb
     target: ext/{{{project}}}.needrestart.conf
   - ext/redhat/preinst.erb
@@ -43,9 +41,6 @@ templates:
   - ext/debian/postinst.erb
   - ext/config/user/conf.d/*.erb
   - ext/bin/*.erb
-  - ext/cli/apps/*.erb
-  - ext/cli/*.erb
-  - ext/cli_defaults/*.erb
   - ext/default.erb
   - ext/ezbake-functions.sh.erb
   - install.sh.erb

resources/puppetlabs/lein-ezbake/template/global/ext/cli/foreground.erb
@@ -1,48 +1,0 @@
-#!/usr/bin/env bash
-
-restartfile="/opt/puppetlabs/server/data/<%= EZBake::Config[:real_name] %>/restartcounter"
-cli_defaults=${INSTALL_DIR}/cli/cli-defaults.sh
-
-if [ ! -e "${INSTALL_DIR}/ezbake-functions.sh" ]; then
-    echo "Unable to find ${INSTALL_DIR}/ezbake-functions.sh script, failing start." 1>&2
-    exit 1
-fi
-
-. "${INSTALL_DIR}/ezbake-functions.sh"
-
-init_restart_file "$restartfile" || exit $?
-
-if !(echo "${@}" | grep -e "--debug" -q)
-then
-    LOG_APPENDER="-Dlogappender=STDOUT"
-fi
-
-CLASSPATH="${INSTALL_DIR}/<%= EZBake::Config[:uberjar_name] %>"
-
-if [ -e "$cli_defaults" ]; then
-  . $cli_defaults
-  if [ $? -ne 0 ]; then
-    echo "Unable to initialize cli defaults, failing start." 1>&2
-    exit 1
-  fi
-fi
-
-COMMAND="${JAVA_BIN} ${JAVA_ARGS} ${LOG_APPENDER} \
-         -cp "$CLASSPATH" \
-         clojure.main -m <%= EZBake::Config[:main_namespace] %> \
-         --config ${CONFIG} --bootstrap-config ${BOOTSTRAP_CONFIG} \
-         --restart-file "${restartfile}" \
-         ${TK_ARGS} \
-         ${@}"
-
-pushd "${INSTALL_DIR}" &> /dev/null
-if [ "$EUID" = "0" ] && command -v runuser &> /dev/null; then
-  runuser "${USER}" -s /bin/bash -c "$COMMAND"
-elif [ "$EUID" = "$(id -u ${USER})" ]; then
-  /bin/bash -c "$COMMAND"
-elif command -v sudo &> /dev/null; then
-  sudo -H -u "${USER}" $COMMAND
-else
-  su "${USER}" -s /bin/bash -c "$COMMAND"
-fi
-popd &> /dev/null

resources/puppetlabs/lein-ezbake/template/global/ext/cli/reload.erb
@@ -1,50 +1,0 @@
-#!/usr/bin/env bash
-set +e
-
-restartfile="/opt/puppetlabs/server/data/<%= EZBake::Config[:real_name] %>/restartcounter"
-reload_timeout="${RELOAD_TIMEOUT:-<%= EZBake::Config[:reload_timeout] %>}"
-timeout="$reload_timeout"
-realname="<%= EZBake::Config[:real_name] %>"
-
-if [ -d "/run" ]; then
-  PIDFILE="/run/puppetlabs/${realname}/${realname}.pid"
-else
-  PIDFILE="/var/run/puppetlabs/${realname}/${realname}.pid"
-fi
-
-if [ ! -e "${INSTALL_DIR}/ezbake-functions.sh" ]; then
-    echo "Unable to find ${INSTALL_DIR}/ezbake-functions.sh script, failing start." 1>&2
-    exit 1
-fi
-
-. "${INSTALL_DIR}/ezbake-functions.sh"
-
-init_restart_file "$restartfile" || exit $?
-
-initial="$(head -n 1 "$restartfile")"
-pid="$(pgrep -f "<%= EZBake::Config[:uberjar_name] %>.* -m <%= EZBake::Config[:main_namespace] %>")"
-kill -HUP $pid >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-    echo "Service not running so cannot be reloaded" 1>&2
-    exit 1
-fi
-sleep 0.1
-cur="$(head -n 1 "$restartfile")"
-while [ "$cur" == "$initial" ] ;do
-    kill -0 $pid >/dev/null 2>&1
-    if [ $? -ne 0 ]; then
-        echo "Process $pid exited before reload had completed" 1>&2
-        rm -f "$PIDFILE"
-        exit 1
-    fi
-    sleep 1
-    cur="$(head -n 1 "$restartfile")"
-
-    ((timeout--))
-    if [ $timeout -eq 0 ]; then
-        echo "Reload timed out after $reload_timeout seconds"
-        exit 1
-    fi
-done
-
-exit 0

resources/puppetlabs/lein-ezbake/template/global/ext/cli/start.erb
@@ -1,120 +1,0 @@
-#!/usr/bin/env bash
-set +e
-
-pid="$(pgrep -f "<%= EZBake::Config[:uberjar_name] %>.* -m <%= EZBake::Config[:main_namespace] %>")"
-
-restartfile="/opt/puppetlabs/server/data/<%= EZBake::Config[:real_name] %>/restartcounter"
-start_timeout="${START_TIMEOUT:-<%= EZBake::Config[:start_timeout] %>}"
-
-real_name="<%= EZBake::Config[:real_name] %>"
-
-if [ -d "/run" ]; then
-  rundir="/run/puppetlabs/${real_name}"
-else
-  rundir="/var/run/puppetlabs/${real_name}"
-fi
-
-app_logdir=${app_logdir:=/var/log/puppetlabs/${real_name}}
-PIDFILE="${rundir}/${real_name}.pid"
-
-cli_defaults=${INSTALL_DIR}/cli/cli-defaults.sh
-
-if [ ! -e "${INSTALL_DIR}/ezbake-functions.sh" ]; then
-    echo "Unable to find ${INSTALL_DIR}/ezbake-functions.sh script, failing start." 1>&2
-    exit 1
-fi
-
-/usr/bin/install --directory --owner=$USER --group=$GROUP --mode=755 "$rundir"
-if [ $? -ne 0 ]; then
-    echo "Unable to create/set permissions for rundir: ${rundir}" 1>&2
-    exit 1
-fi
-
-. "${INSTALL_DIR}/ezbake-functions.sh"
-
-write_pid_file() {
-    echo "$pid" > "$PIDFILE"
-    if [ $? -ne 0 ]; then
-        echo "Unable to write pid file: ${PIDFILE}" 1>&2
-        terminate_java_process
-        exit 1
-    fi
-}
-
-terminate_java_process() {
-    echo "Startup script was terminated before completion" 1>&2
-    kill_pid "$pid" "$PIDFILE" "$SERVICE_STOP_RETRIES"
-    exit 1
-}
-
-if [ -n "$pid" ]; then
-    write_pid_file
-    exit 0
-fi
-
-rm -f "$PIDFILE"
-
-init_restart_file "$restartfile" || exit $?
-
-CLASSPATH=${INSTALL_DIR}/<%= EZBake::Config[:uberjar_name] %>
-
-if [ -e "$cli_defaults" ]; then
-  . $cli_defaults
-  if [ $? -ne 0 ]; then
-    echo "Unable to initialize cli defaults, failing start." 1>&2
-    exit 1
-  fi
-fi
-
-java_version=$($JAVA_BIN -version 2>&1 | head -1 | awk -F\" '{ print $2 }')
-java_major_version=$(echo $java_version | awk -F. '{ print $1 }')
-
-LOG_APPENDER="-Dlogappender=F1"
-
-out_of_memory_flag='-XX:OnOutOfMemoryError=kill -9 %p'
-if [ "$java_major_version" -ge 11 ]; then
-  out_of_memory_flag="-XX:+CrashOnOutOfMemoryError"
-fi
-
-"$JAVA_BIN" $JAVA_ARGS $LOG_APPENDER \
-  "$out_of_memory_flag" \
-  -XX:ErrorFile="$app_logdir/${real_name}_err_pid%p.log" \
-  -cp "$CLASSPATH" \
-  clojure.main \
-  -m <%= EZBake::Config[:main_namespace] %> \
-  --config "$CONFIG" \
-  --bootstrap-config "$BOOTSTRAP_CONFIG" \
-  --restart-file "$restartfile" \
-  $TK_ARGS &
-
-# $! is the process id of the last backgrounded process, the Java process above.
-pid=$!
-trap terminate_java_process SIGHUP SIGINT SIGTERM
-write_pid_file
-
-cur="$(head -n 1 "$restartfile")"
-initial="$cur"
-
-timeout="$start_timeout"
-while [ "$cur" == "$initial" ] ;do
-    kill -0 $pid >/dev/null 2>&1
-    if [ $? -ne 0 ]; then
-        rm -f "$PIDFILE"
-        echo "Background process $pid exited before start had completed" 1>&2
-        exit 1
-    fi
-
-    sleep 1
-    cur="$(head -n 1 "$restartfile")"
-
-    ((timeout--))
-    if [ $timeout -eq 0 ]; then
-        echo "Startup timed out after $start_timeout seconds" 1>&2
-        terminate_java_process
-        rm -f "$PIDFILE"
-        exit 1
-    fi
-done
-
-write_pid_file
-exit 0

resources/puppetlabs/lein-ezbake/template/global/ext/cli/stop.erb
@@ -1,26 +1,0 @@
-#!/usr/bin/env bash
-set +e
-
-pid="$(pgrep -f "<%= EZBake::Config[:uberjar_name] %>.* -m <%= EZBake::Config[:main_namespace] %>")"
-realname="<%= EZBake::Config[:real_name] %>"
-
-if [ -d "/run" ]; then
-  PIDFILE="/run/puppetlabs/${realname}/${realname}.pid"
-else
-  PIDFILE="/var/run/puppetlabs/${realname}/${realname}.pid"
-fi
-
-if [ ! -e "${INSTALL_DIR}/ezbake-functions.sh" ]; then
-    echo "Unable to find ${INSTALL_DIR}/ezbake-functions.sh script, failing stop." 1>&2
-    exit 1
-fi
-
-. "${INSTALL_DIR}/ezbake-functions.sh"
-
-if [ -z "$pid" ]; then
-    rm -f "$PIDFILE"
-    exit 0
-else
-    kill_pid "$pid" "$PIDFILE" "$SERVICE_STOP_RETRIES"
-    exit $?
-fi

resources/puppetlabs/lein-ezbake/template/global/ext/ezbake.tmpfiles.conf.erb
@@ -1,1 +1,0 @@
-d /run/puppetlabs/<%= EZBake::Config[:real_name] %> 0755 <%= EZBake::Config[:user] %> <%= EZBake::Config[:group] %> -

commit 70a576d27e482ea1bef0521f9abcac967fa015a9

ezbake: Introduce service-port variable
This adds a bit of a startup delay to ensure that systemd not just
starts the process and thinks the service is already up and running.

Previously this was handled by the wrapper script and Type=forking. This
commit fixes a regression from https://github.com/OpenVoxProject/ezbake/commit/b2de7c7a2e7704491a08665a9f53ded8009aa109

This also requires changes to the projects to provide the actual port.

* https://github.com/OpenVoxProject/openvox-server/pull/80
* https://github.com/OpenVoxProject/openvoxdb/pull/69

This is just a workaround and not a perfect solution. For systemd-based
systemd we should implement sd_notify and switch to Type=notify-reload.

https://www.freedesktop.org/software/systemd/man/latest/systemd.service.html#Type=

resources/puppetlabs/lein-ezbake/staging-templates/ezbake.rb.mustache
@@ -1,5 +1,6 @@
 module EZBake
   Config = {
+      :service_port   => {{{service-port}}},
       :project        => {{{project}}},
       :version        => {{{packaging-version}}},
       :release        => {{{packaging-release}}},

resources/puppetlabs/lein-ezbake/template/global/ext/redhat/ezbake.service.erb
@@ -63,6 +63,10 @@ ExecStart=<%= EZBake::Config[:java_bin] %> $JAVA_ARGS -Dlogappender=F1 \
 
 KillMode=process
 
+<% if EZBake::Config[:service_port] -%>
+# Wait until the service is started and opened the correct TCP port
+ExecStartPost=timeout 30s sh -c 'while ! ss -H -t -l -n sport = :<%= EZBake::Config[:service_port] %> | grep -q "^LISTEN.*:<%= EZBake::Config[:service_port] %>"; do sleep 1s; done'
+<% end -%>
 <% EZBake::Config[:redhat][:post_start_action].each do |action| -%>
 ExecStartPost=-<%= action %>
 <% end -%>

src/puppetlabs/ezbake/core.clj
@@ -71,6 +71,7 @@
 (def LocalProjectVars
   {(schema/optional-key :user) schema/Str
    (schema/optional-key :numeric-uid-gid) schema/Int
+   (schema/optional-key :service-port) schema/Int
    (schema/optional-key :group) schema/Str
    (schema/optional-key :puppet-platform-version) schema/Int
    (schema/optional-key :bootstrap-source) BootstrapSource
@@ -608,6 +609,7 @@ Additional uberjar dependencies:
      :real-name                          (-> (:name lein-project) get-real-name as-ruby-literal)
      :user                               (local->ruby :user (:name lein-project))
      :numeric-uid-gid                    (local->ruby :numeric-uid-gid nil)
+     :service-port                       (local->ruby :service-port nil)
      :group                              (local->ruby :group (:name lein-project))
      :uberjar-name                       (as-ruby-literal (:uberjar-name lein-project))
      :config-files                       (as-ruby-literal (map remove-erb-extension config-files))

test/unit/puppetlabs/ezbake/core_test.clj
@@ -32,6 +32,7 @@
 (deftest trivial-make-template-map-behavior
   (testing "keyword bootstrap-source value with hyphens is ok"
     (let [expected {:numeric-uid-gid "nil"
+                    :service-port "nil"
                     :debian-prerm "[]"
                     :config-files "[]"
                     :main-namespace "'puppetlabs.trapperkeeper.main'"