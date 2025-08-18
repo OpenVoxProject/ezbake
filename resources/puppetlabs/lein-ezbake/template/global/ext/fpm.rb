b7f6c2526cb3758881b4a185f86cde897ec24f80
Robert Waffen <rw@betadots.de>
2025-08-18 15:28:50 +0200

    fix: improve error handling in FPM output capture

--- resources/puppetlabs/lein-ezbake/template/global/ext/fpm.rb
+++ resources/puppetlabs/lein-ezbake/template/global/ext/fpm.rb
@@ -473,8 +473,8 @@ end
 
 patch_files(options) do
   # fpm sends all output to stdout
-  out, _, stat = Open3.capture3("#{fpm_editor} fpm #{fpm_opts.join(' ')}")
-  fail "Error trying to run FPM for #{options.dist}!\n#{out}" unless stat.success?
+  out, err, stat = Open3.capture3("#{fpm_editor} fpm #{fpm_opts.join(' ')}")
+  fail "Error trying to run FPM for #{options.dist}!\n stderr: #{err}\n stdout: #{out}\n" unless stat.success?
 
   puts "#{out}"
 
