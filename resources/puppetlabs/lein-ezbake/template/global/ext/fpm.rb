4ec0807cd052136a11af8a214ae1014fa9603262
nmburgan <13688219+nmburgan@users.noreply.github.com>
2026-09-15 23:43:07 +0000

    Stop pointing deb packages at an architecture specific java path
    The deb builds set java_bin to /usr/lib/jvm/java-25-openjdk-amd64/bin/java (and the java-21 equivalent for older platforms), which patch_files writes into the systemd unit and the defaults file. The packages are actually noarch, so on arm64 hosts that path does not exist and the service can't start.
    
    For the moment, this reverts the deb packages to use the /usr/bin/java alternatives symlink. Not sure yet how we'll solve this. We might have to make arch-specific packages for deb, and it might be something to tackle once we move to vangaon.
    
    Signed-off-by: nmburgan <13688219+nmburgan@users.noreply.github.com>

--- resources/puppetlabs/lein-ezbake/template/global/ext/fpm.rb
+++ resources/puppetlabs/lein-ezbake/template/global/ext/fpm.rb
@@ -392,16 +392,17 @@ elsif options.output_type == 'deb'
     fpm_opts << "--deb-activate #{trigger}"
   end
 
-  # figure out correct java dependency
+  # figure out correct java dependency. The java binary stays at the
+  # /usr/bin/java alternatives symlink because the Debian JVM directories
+  # embed the architecture, for example java-25-openjdk-amd64, and these
+  # packages are built as Architecture: all.
   case options.dist
   # Focal Fossa,
   when 'ubuntu20.04'
     options.java = 'openjdk-21-jre-headless'
-    options.java_bin = '/usr/lib/jvm/java-21-openjdk-amd64/bin/java'
   # Trixie, Forky, Noble Numbat, Plucky Puffin, Questing Quokka, Resolute Raccoon, Stonking Stingray
   when 'debian13', 'debian14', 'ubuntu22.04', 'ubuntu24.04', 'ubuntu25.04', 'ubuntu25.10', 'ubuntu26.04', 'ubuntu26.10'
     options.java = 'openjdk-25-jre-headless'
-    options.java_bin = '/usr/lib/jvm/java-25-openjdk-amd64/bin/java'
   else
     fail "no matching OS data found for #{options.dist}"
   end
